0778f56accdd753c9efb2d1c23466ebf09a411c6
Damien Pontifex <damien.pontifex@gmail.com>
2020-12-12 18:43:51 +0800

    common client setup method

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -9,10 +9,14 @@ fn test_config() -> (String, String, String) {
     )
 }
 
-#[tokio::test]
-async fn test_connect() {
+async fn client() -> azure_iot_sdk::Result<IoTHubClient<MqttTransport>> {
     let (hostname, device_id, access_key) = test_config();
     let token_source = DeviceKeyTokenSource::new(&hostname, &device_id, &access_key).unwrap();
-    let client = IoTHubClient::<MqttTransport>::new(&hostname, device_id, token_source).await;
+    IoTHubClient::<MqttTransport>::new(&hostname, device_id, token_source).await
+}
+
+#[tokio::test]
+async fn test_connect() {
+    let client = client().await;
     assert!(client.is_ok());
 }
