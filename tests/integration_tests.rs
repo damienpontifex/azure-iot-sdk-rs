f1ac0b6d0e5b7f85216833a09c63ffc0d3d783b6
Damien Pontifex <damien.pontifex@gmail.com>
2020-12-13 09:21:44 +0800

    Integration test for responding to direct methods
    - Currently doesn't work because of mismatch with tokio 0.2 and 0.3
      which I think we're waiting for hyper upgrade so the azure sdk can
      upgrade to tokio 0.3

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,4 +1,6 @@
-use azure_iot_sdk::{DeviceKeyTokenSource, IoTHubClient};
+use azure_iot_sdk::{DeviceKeyTokenSource, DirectMethodResponse, IoTHubClient, MessageType};
+use iothub::service::ServiceClient;
+use serde_json::json;
 use std::env;
 
 fn test_config() -> (String, String, String) {
@@ -19,4 +21,42 @@ async fn client() -> azure_iot_sdk::Result<IoTHubClient> {
 async fn test_connect() {
     let client = client().await;
     assert!(client.is_ok());
+}
+
+#[tokio::test]
+async fn test_direct_method() {
+    let service_client = ServiceClient::from_connection_string("", 3600).unwrap();
+
+    let direct_method = service_client.create_device_method("device_id", "method_name", 30, 30);
+
+    let mut client = client().await.unwrap();
+    let mut receiver = client.get_receiver().await;
+    tokio::task::spawn(async move {
+        loop {
+            while let Some(msg) = receiver.recv().await {
+                match msg {
+                    MessageType::DirectMethod(msg) => {
+                        client
+                            .respond_to_direct_method(DirectMethodResponse::new(
+                                msg.request_id,
+                                0,
+                                Some("test_direct_method response".to_string()),
+                            ))
+                            .await
+                            .unwrap();
+                    }
+                    _ => println!("Received other event"),
+                }
+            }
+        }
+    });
+
+    let response = direct_method.execute(json!({ "key": "value" })).await;
+    assert!(response.is_ok());
+    let response = response.unwrap();
+    assert_eq!(response.status, 0);
+    assert_eq!(
+        response.payload,
+        Some("test_direct_method response".to_string())
+    );
 }
